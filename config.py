f0574fabd29507284a5b39ef7bf6a308c761b5cd
Travis Beatty <travisby@gmail.com>
2013-05-03 21:24:57 -0400

    Added some nice configuration settings for later.

---
+++ config.py
@@ -0,0 +1,12 @@
+CONNECTION_TUPLES = [
+    ('hackathon.hopto.org', 31671),
+    ('hackathon.hopto.org', 31672)
+]
+TEAM = 'Kronos'
+
+INIT = 'INIT'
+RECEIVE = 'RECD'
+START = 'START'
+STOP = 'STOP'
+BEGIN = '%(init)s %(team)s' % {'init': INIT, 'team': TEAM}
+MAX_PACKET_SIZE = 999
