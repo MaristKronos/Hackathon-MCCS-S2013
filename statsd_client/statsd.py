07ef8709d3519dff3a64d9ae63b43c306d55e5e8
Travis Beatty <travisby@gmail.com>
2013-05-04 16:23:33 -0400

    Created our socket connection for StatsD

--- statsd_client/statsd.py
+++ statsd_client/statsd.py
@@ -9,9 +9,13 @@ import socket
 
 class Statsd(object):
     _statsd = None
+    _enc = ''
 
-    def __init__(self, host=config.host, port=config.port):
-        pass
+    def __init__(self, host=config.host, port=config.port, enc=config.enc_key):
+        self._statsd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._.bind((host, port))
+        self._enc = enc
 
     def __del__(self):
-        pass
+        self._stastd.shutdown(socket.SHUT_RDWR)
+        self._stastd.close()
