44e185290974e30fe8ce18d970890fefc1396a04
Travis Beatty <travisby@gmail.com>
2013-05-04 11:43:39 -0400

    Added a new loop construct

--- runner.py
+++ runner.py
@@ -2,10 +2,8 @@ from hermes_client.client import Hermes_Client
 
 client = Hermes_Client()
 i = 0
-while True:
+while client.next_turn():
     i += 1
-    if i == 60:
-        print client.next_turn()
-        i = 0
-    else:
-        client.next_turn()
+    message = client.get_stats()
+    if not i % 60:
+        print message
