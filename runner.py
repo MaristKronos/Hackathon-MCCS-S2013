04d1c24c3894da5ba17d780ef3b840eb275e7c13
Travis Beatty <travisby@gmail.com>
2013-05-05 15:43:05 -0400

    Changed how we calculate load

--- runner.py
+++ runner.py
@@ -30,39 +30,39 @@ while client.next_turn():
     x.update_value('profit.total', message[2]['total_profit'], int(time.time()))
 
     try:
-        x.update_value('load.web.na', client._config['web_NA_total'] * 180/message[1]['NA_web'], int(time.time()))
+        x.update_value('load.web.na',  message[1]['NA_web'] / client._config['web_NA_total'], int(time.time()))
     except ZeroDivisionError:
         pass
     try:
-        x.update_value('load.web.eu', client._config['web_EU_total'] * 180/message[1]['EU_web'], int(time.time()))
+        x.update_value('load.web.eu', message[1]['EU_web'] / client._config['web_EU_total'], int(time.time()))
     except ZeroDivisionError:
         pass
     try:
-        x.update_value('load.web.ap', client._config['web_AP_total'] * 180/message[1]['AP_web'], int(time.time()))
+        x.update_value('load.web.ap', message[1]['AP_web'] / client._config['web_AP_total'], int(time.time()))
     except ZeroDivisionError:
         pass
     try:
-        x.update_value('load.java.na', client._config['java_NA_total'] * 400/message[1]['NA_java'], int(time.time()))
+        x.update_value('load.java.na', message[1]['NA_java'] / client._config['java_NA_total'], int(time.time()))
     except ZeroDivisionError:
         pass
     try:
-        x.update_value('load.java.eu', client._config['java_EU_total'] * 400/message[1]['EU_java'], int(time.time()))
+        x.update_value('load.java.eu', message[1]['EU_java'] / client._config['java_EU_total'], int(time.time()))
     except ZeroDivisionError:
         pass
     try:
-        x.update_value('load.java.ap', client._config['java_AP_total'] * 400/message[1]['AP_java'], int(time.time()))
+        x.update_value('load.java.ap', message[1]['AP_java'] / client._config['java_AP_total'], int(time.time()))
     except ZeroDivisionError:
         pass
     try:
-        x.update_value('load.db.na', client._config['db_NA_total'] * 1000/message[1]['NA_db'], int(time.time()))
+        x.update_value('load.db.na', message[1]['NA_db'] / client._config['db_NA_total'], int(time.time()))
     except ZeroDivisionError:
         pass
     try:
-        x.update_value('load.db.eu', client._config['db_EU_total'] * 1000/message[1]['EU_db'], int(time.time()))
+        x.update_value('load.db.eu', message[1]['EU_db'] / client._config['db_EU_total'], int(time.time()))
     except ZeroDivisionError:
         pass
     try:
-        x.update_value('load.db.ap', client._config['db_AP_total'] * 1000/message[1]['AP_db'], int(time.time()))
+        x.update_value('load.db.ap', message[1]['AP_db'] / client._config['db_AP_total'], int(time.time()))
     except ZeroDivisionError:
         pass
 
